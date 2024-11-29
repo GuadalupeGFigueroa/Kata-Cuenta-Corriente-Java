9e309796105ce88fa365d786cfb0cdccc1c9fbea
GuadalupeGFigueroa <guadalupe.g.figueroa@gmail.com>
2024-11-30 00:03:01 +0100

    Dependencies added to POM file

---
+++ src/main/java/dev/guadalupe/kata_cuenta_corriente_java/KataCuentaCorrienteJavaApplication.java
@@ -0,0 +1,13 @@
+package dev.guadalupe.kata_cuenta_corriente_java;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KataCuentaCorrienteJavaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KataCuentaCorrienteJavaApplication.class, args);
+	}
+
+}
